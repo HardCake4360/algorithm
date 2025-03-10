72f74e791c73feb0571fd097e2cb7129459fd020
HardCake4360 <chriskim4360@gmail.com>
2025-03-10 17:13:18 +0900

    primeNumber
    에라토스테네스의 체를 이용한 소수 구하기

--- algorithm/algorithm0306.py
+++ algorithm/algorithm0306.py
@@ -9,6 +9,53 @@ def oneToNRec(n):
         return 1
     return n+oneToNRec(n-1)
 
+def minMax(list):
+    min=list[0]
+    max=min
+    for i in list[1:]:
+        if(min>i):
+            min=i
+            continue
+        if(max<i):
+            max=i
+    return min,max
+
+def isPrime(x):
+    for i in range(2,x//2+1):
+        if(x%i == 0):
+            return False
+    return True
+
+def PrimeListMaker(stop):
+    list = [True]*(stop+1)
+    list[0],list[1]=False,False
+    for i in range(2,stop+1):
+        if(list[i] is False):
+           continue
+        if(isPrime(i)):
+            for j in range(2*i,stop,i):
+                list[j]=False
+                print("%d is false"%j)
+        else :
+            list[i]=False
+            print("%d is false"%i)
+    return list
+
+def primeCheck(list):
+    sum = 0
+    for i in range(len(list)):
+        if(list[i]):
+            sum+=1
+    return sum
+
+
 if __name__ == '__main__':
     print(oneToN(4))
     print(oneToNRec(10))
+    arr=[1,2,3,4,5,6,7,8,9,10]
+    print(minMax(arr))
+    
+    primeList = PrimeListMaker(10)
+    print(primeCheck(primeList))
+
+
