33dfa165bedd9c0abd30ca14b2af9577dd0ed75a
HardCake4360 <76778281+HardCake4360@users.noreply.github.com>
2025-06-05 15:50:15 +0900

    0605 수업내용

--- problemSolving/0529.py
+++ problemSolving/0529.py
@@ -75,6 +75,43 @@ def BABBA_BU(n):
     a,b = val[n]
     return (a,b)
 
+#0605 수업내용
+"""
+-기말 대책
+연습문제 풀어보기
+다빈치 코딩 알고리즘에 나온거
+전부 코드 시험
+그래프 내용
+중간고사 내용은 안나옴
+
+점화식으로 표현 가능한가? -> 다이나믹 프로그래밍으로 풀기 + memoization + bottom up 방식
+다이나믹 프로그래밍 부분에서 최소 하나는 나옴옴
+"""
+#평범한 배낭 문제
+def solve(n,k):
+    if k<0 or k<W[n]: return 0
+    case1 = solve(n-1, k) +V[n]
+    case2 = solve(n-1,k)
+    return max(case1,case2)
+
+#동전 문제
+def coin(coins:list, val):
+    coins[1]*val/coin[1] + coins[2]*val/coin[2] + ... + coins[n]*val/coins[n]
+
+    for coin in coins:
+        for i in range(0,val/coin+1):
+            coin*i
+
+#조약돌 문제
+#RGB거리
+
+"""
+-시험에 나올만한 문제
+분할정복: 쿼드 트리 문제
+백 트래킹: 근손실 문제
+누적합: 다이나믹 프로그래밍 적 해석
+그리디 알고리즘
+"""
 
 
 if __name__ == "__main__":
